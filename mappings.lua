5cfd0a2ce23a13a9aec4296ac2e651a62858247c
YJack0000 <yjack0000@cerana.tech>
2023-12-06 02:51:43 +0800

    feat: make the mapping with lazygit

--- mappings.lua
+++ mappings.lua
@@ -2,12 +2,48 @@
 local M = {}
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-  },
-  v = {
-    [">"] = { ">gv", "indent" },
-  },
+    n = {
+        [";"] = { ":", "enter command mode", opts = { nowait = true } },
+        ["<leader>lg"] = {
+            function()
+                require("nvterm").setup {
+                    terminals = {
+                        shell = vim.o.shell,
+                        list = {},
+                        type_opts = {
+                            float = {
+                                relative = "editor",
+                                row = 0.1,
+                                col = 0.1,
+                                width = 0.8,
+                                height = 0.8,
+                                border = "single",
+                            },
+                            horizontal = { location = "rightbelow", split_ratio = 0.3 },
+                            vertical = { location = "rightbelow", split_ratio = 0.5 },
+                        },
+                    },
+                    behavior = {
+                        autoclose_on_quit = {
+                            enabled = false,
+                            confirm = true,
+                        },
+                        close_on_exit = true,
+                        auto_insert = true,
+                    },
+                }
+
+                local terminal = require("nvterm.terminal")
+                terminal.send("lazygit", "float")
+                terminal.toggle "float"
+                terminal.toggle "float"
+            end,
+            "Toggle lazygit",
+        },
+    },
+    v = {
+        [">"] = { ">gv", "indent" },
+    },
 }
 
 return M
