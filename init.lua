adfa65c19a5a6e4286b77655267d12f813a0656c
YJack0000 <jackleagends@gmail.com>
2024-01-28 20:14:38 +0800

    style: just remove ()

--- init.lua
+++ init.lua
@@ -7,13 +7,13 @@
 -- })
 
 -- make tab into 4 spaces
-vim.cmd("set tabstop=4")
+vim.cmd "set tabstop=4"
 -- make format use 4 spaces
-vim.cmd("set shiftwidth=4")
+vim.cmd "set shiftwidth=4"
 -- make line number to relative
-vim.cmd("set relativenumber")
+vim.cmd "set relativenumber"
 
 -- set default shell to zsh if it exists
-if vim.fn.executable("/usr/bin/zsh") == 1 then
-  vim.o.shell = "/usr/bin/zsh"
+if vim.fn.executable "/usr/bin/zsh" == 1 then
+    vim.o.shell = "/usr/bin/zsh"
 end
