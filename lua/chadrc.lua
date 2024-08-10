39a023f194200f69e6fa9c57e46f48550734d7a3
YJack0000 <jackleagends@gmail.com>
2024-08-11 01:55:43 +0800

    feat: update chad version

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -4,23 +4,24 @@ local M = {}
 local highlights = require("highlights")
 
 M.ui = {
-	theme = "github_dark",
-	theme_toggle = { "github_dark", "github_dark" },
-
-	hl_override = highlights.override,
-	hl_add = highlights.add,
 	lsp = {
 		signature = true,
 	},
-
 	tabufline = {
 		-- order = { "treeOffset", "buffers", "tabs", "btns"},
-		order = { "treeOffset", "buffers", "tabs"},
-        -- enabled = false,
+		order = { "treeOffset", "buffers", "tabs" },
+		-- enabled = false,
 		modules = {
 			-- You can add your custom component
 		},
 	},
+}
+
+M.base46 = {
+	theme = "onedark",
+	theme_toggle = { "onedark", "one_light" },
+	hl_override = highlights.override,
+	hl_add = highlights.add,
 }
 
 return M
